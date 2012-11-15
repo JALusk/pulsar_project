4f21d8ca0bfea193d42a353f1909c0f513387c93
Jeremy Lusk <Jeremy.Lusk@Gmail.com>
2012-11-15 09:21:19 -0600

    added a plot_period.py to create a time series histogram

--- plot_image.py
+++ plot_image.py
@@ -10,8 +10,16 @@ pulsar_data = 'pulsar3.lis'
 # read_pulsar returns time (t), x, y, and energy (E) as vectors
 t, x, y, E = read_pulsar(pulsar_data)
 
+xmin = x.min()
+xmax = x.max()
+ymin = y.min()
+ymax = y.max()
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(x,y, ',')
-ax.set_title('X-ray image data, Pulsar 3')
-plt.show()
+hist = ax.hist2d(x,y,bins=512,cmap=cm.hot_r)
+ax.set_title('X-ray image data, Pulsar 3: binned 512x512')
+#cb = colorbar(hist[0])
+#cb.set_label('counts')
+show()
+fig.savefig('hist')
