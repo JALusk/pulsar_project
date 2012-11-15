5ca7f5337a7d097925eb8bae8f0ae7ee6f4c5612
Jeremy Lusk <Jeremy.Lusk@Gmail.com>
2012-11-15 09:40:16 -0600

    Modified read_pulsar to filter out bad photons

--- read_pulsar.py
+++ read_pulsar.py
@@ -1,18 +1,26 @@
 # read_pulsar.py
 #
 # Routine to read in the data from pulsar3.lis and save the 
-# columns as vectors in python
+# columns as vectors in python. Filter out the bad photons
 
 import scipy
 
 def read_pulsar(filename):
   data = scipy.loadtxt(filename, skiprows = 2)
   
-  time = data[:,1]
+  t = data[:,1]
   x = data[:,2]
   y = data[:,3]
   E = data[:,4]
+  flag = data[:,5]
+  
+  # Filter out the bad photons (flag not equal zero)
+  # and create lists of good photon data
+  for i in range(0, len(t)):
+    if flag[i] = 0:
+      t_good.append(t[i])
+      x_good.append(x[i])
+      y_good.append(y[i])
+      E_good.append(E[i])
 
-  type(x)
-
-  return time, x, y, E
+  return t_good, x_good, y_good, E_good
